873b2f0adc7b68982e31babd891fc6c4d8916a13
Keli Booster <kbooster17@gmail.com>
2020-11-26 01:25:32 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
       resources :users
       resources :houses
       resources :favorites, only: [:destroy, :create]
-     
-      post '/signin', to: 'user_token#create'
-      post '/signup', to: 'users#create' 
+
+
+      post '/create/signup', to: 'users#create' 
+      post '/auth/signin', to: 'user_token#create'
+      get '/dashboard/:username', to: 'users#find_user', as: 'user_detail'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
