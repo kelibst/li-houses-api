15e976dd528020ee0fceea09310f9deb36ce62eb
Keli Booster <kbooster17@gmail.com>
2020-11-26 01:16:34 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
      
       post '/signin', to: 'user_token#create'
       post '/signup', to: 'users#create' 
-      get '/current/:username', to: 'users#find_user'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
