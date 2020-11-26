cf07d0c979a1adf278d09c1415a8b8e65a1544ee
Keli Booster <kbooster17@gmail.com>
2020-11-26 01:35:41 +0000

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'homepage/home'
 
   namespace :api do
-    namespace :v1 do
+    namespace :v1 do 
+      get '/dashboard/:username', to: 'users#find_user', as: 'user_detail'
       resources :users
       resources :houses
       resources :favorites, only: [:destroy, :create]
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
 
       post '/create/signup', to: 'users#create' 
       post '/auth/signin', to: 'user_token#create'
-      get '/dashboard/:username', to: 'users#find_user', as: 'user_detail'
+     
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
