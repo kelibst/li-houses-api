c5a603f8f797b166362772afdcd5983ba1addb21
Keli Booster <kbooster17@gmail.com>
2020-12-17 13:25:32 +0000

    add necessary routes test

--- spec/houses/routes_spec.rb
+++ spec/houses/routes_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 describe 'Routes', type: :routing do
-  describe 'usersController' do
+  describe 'housesController' do
     it 'routes get houses to index action' do
       expect(get: '/api/v1/houses').to route_to(
         controller: 'api/v1/houses',
@@ -34,6 +34,46 @@ describe 'Routes', type: :routing do
     it 'routes delete request with id to houses delete action' do
       expect(DELETE: '/api/v1/houses/:id').to route_to(
         controller: 'api/v1/houses',
+        action: 'destroy',
+        id: ':id'
+      )
+    end
+  end
+
+  describe 'usersController' do
+    it 'routes get request to users index action' do
+      expect(get: '/api/v1/users/').to route_to(
+        controller: 'api/v1/users',
+        action: 'index'
+      )
+    end
+
+    it 'routes post request to users create action' do
+      expect(post: '/api/v1/users/').to route_to(
+        controller: 'api/v1/users',
+        action: 'create'
+      )
+    end
+
+    it 'routes get request with id to users show action' do
+      expect(get: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
+        action: 'show',
+        id: ':id'
+      )
+    end
+
+    it 'routes put request with id to users update action' do
+      expect(put: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
+        action: 'update',
+        id: ':id'
+      )
+    end
+
+    it 'routes delete request with id to users destroy action' do
+      expect(DELETE: '/api/v1/users/:id').to route_to(
+        controller: 'api/v1/users',
         action: 'destroy',
         id: ':id'
       )
